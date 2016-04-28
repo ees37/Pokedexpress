73d1c3e583b554a2e932074ca4e444db1d4a0380
Mark Archual <mja61@zips.uakron.edu>
2016-04-28 16:05:14 -0400

    Added sql injection prevention for SELECT statements

--- Pokedexpress/API/app.js
+++ Pokedexpress/API/app.js
@@ -1,3 +1,4 @@
+//requires
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,9 +6,12 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 var mysql = require('mysql');
+
+//Fire up Express
 var app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
@@ -26,11 +30,13 @@ var connection = mysql.createConnection({
     database: 'pokedexpress'
 });
 
+// make sure the connection is good
 connection.connect(function(err){
     if (err) throw err;
     console.log('Connected!')
 });
 
+//Port to listen on
 app.listen(8008);
 
 app.use('/home', function(request, response, next){
@@ -38,20 +44,45 @@ app.use('/home', function(request, response, next){
     next();
 });
 
-/* GET home page. */
+/* HTTP GET for placing database information on the home page. */
 app.get('/home', function(request, response)
 {
     console.log('Inside GET');
-    connection.query(request.query["sqlQuery"].toString(), function(error, results, fields)
+    var query = request.query['sqlQuery'].toString().split(" ");  //Split the users input at blankspaces
+    var query_string = 'SELECT * FROM '; //Every query needs to at least have this information to start
+
+    for (i = 0; i < query.length; i++) //Now we need to build our query and guard against injections
+    {
+        if (i == 0)
+        {
+            query_string = query_string + query[i]; //Append the table name
+        }
+        else if (query[i])
+        {
+            if (i < 2)
+            {
+                //If a specific value is specified, we need to add the WHERE clause
+                query_string = query_string + ' WHERE identifier = ' + mysql.escape(query[i]);
+                //The escape function guards against injection attacks
+            }
+            else
+            {
+                //If two or more specific values are entered
+                query_string = query_string + ' OR identifier = ' + mysql.escape(query[i]);
+            }
+        }
+
+    }
+
+    //Now we are ready to query the database
+    connection.query(query_string, function(error, results, fields)
     {
         if(error)
         {
             response.status(500).send(error);
             console.log(error.toString())
         }
-        console.log(results);
         response.send(results);
-
     });
 });
 
@@ -87,6 +118,6 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+module.exports = app;  //Just in case we want to use this elsewhere
 
 
